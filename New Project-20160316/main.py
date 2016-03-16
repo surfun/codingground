adbc5ab9f72e4e2838975006ad8a2b0ead02028b
surfun <surendra2k7@yahoo.co.in>
2016-03-16 16:28:07 +0530

    Created by Coding Ground Client

---
+++ New Project-20160316/main.py
@@ -0,0 +1,20 @@
+# Hello World program in Python
+    
+print "Hello World!\n"
+
+str="Surendra"
+
+print "May Name is  : ",str
+
+print str
+print str[0]
+print str[2:5]
+print str[0:-2]
+print str * 2
+print str + "TEST"
+
+#raw_input("\nPress the Enter key to continue :")
+
+#a=b=d=1
+#c="jhony"
+#print a, b, c, d
