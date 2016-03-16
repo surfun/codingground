9468a1a8bedcbd49b3ee3fcf928a462c3498f6de
surfun <surendra2k7@yahoo.co.in>
2016-03-16 16:29:20 +0530

    Created by Coding Ground Client

---
+++ Python-Practice/Dictionary.py
@@ -0,0 +1,12 @@
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name':'jhon','code':3214,'dept':'sales'}
+
+print dict
+print dict['one']
+print dict[2]
+print tinydict
+print "All the keys",tinydict.keys()
+print "All the values",tinydict.values()
