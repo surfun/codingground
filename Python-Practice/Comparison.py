41309592de0c56c5d94e17f469fa1c7236ab7403
surfun <surendra2k7@yahoo.co.in>
2016-03-16 16:44:18 +0530

    Created by Coding Ground Client

---
+++ Python-Practice/Comparison.py
@@ -0,0 +1,43 @@
+a=10
+b=20
+c=0
+
+print "a = ",a,"\nb = ",b,"\nc = ",c
+if ( a == b):
+    print "Line1 - a equal to b "
+else :
+    print "Line1 - a is not equal to b"
+
+if ( a != b ):
+    print "Line2 - a is not equal to b"
+else:
+    print "Line2 - a is equal to b"
+    
+if ( a <> b ):
+   print "Line 3 - a is not equal to b"
+else:
+   print "Line 3 - a is equal to b"
+
+if ( a < b ):
+   print "Line 4 - a is less than b" 
+else:
+   print "Line 4 - a is not less than b"
+
+if ( a > b ):
+   print "Line 5 - a is greater than b"
+else:
+   print "Line 5 - a is not greater than b"
+   
+a = 5;
+b = 20;
+print "a = ",a,"\nb = ",b
+
+if ( a <= b ):
+   print "Line 6 - a is either less than or equal to  b"
+else:
+   print "Line 6 - a is neither less than nor equal to  b"
+
+if ( b >= a ):
+   print "Line 7 - b is either greater than  or equal to b"
+else:
+   print "Line 7 - b is neither greater than  nor equal to b"
