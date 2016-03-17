b0580c9d1a630d10f7bd57a8fbe027d2339a0a4b
surfun <surendra2k7@yahoo.co.in>
2016-03-17 16:06:47 +0530

    Created by Coding Ground Client

---
+++ Python-Practice/Decision.py
@@ -0,0 +1,22 @@
+var = 100
+
+if (var == 100 ): print "Value of expression is 100"
+
+
+var1=100
+if var1:
+    print "Just checking the value in var1"
+    print var1
+else:
+   print "Got a false expression value for var1"
+   print var1
+   
+var2=0
+if var2:
+    print "just checking value of Var2"
+    print var2
+else:
+   print "Got a false expression value for var2"
+   print var2
+   
+print "Good Bye"
